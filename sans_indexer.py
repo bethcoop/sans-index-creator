03d65ebfbc27afdff48c8ae6a0fcd04923968d0b
bethcoop <bethany.c101@outlook.com>
2023-07-15 00:40:50 -0700

    Added additional processing

--- sans_indexer.py
+++ sans_indexer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+import string
 
 import requests as rq
 import argparse
 import sys
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import re
 
 # Parse input
 Usage = ("""{}SANS Txt to Index
@@ -36,47 +40,54 @@ if options.student_name:
 common_word_mode = True
 if options.mode and options.mode == 'f':
     print("NLTK stop word removal mode")
-    stop_words = set(stopwords.words('english'))
     common_word_mode = False
 else:
     print("Common word removal mode")
-    common_words = rq.get("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt").text.split("\n")
+    common_words = rq.get(
+        "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt").text.split(
+        "\n")
 # function to recursively strip given characters in a word
-characters_to_strip = "()'\":,”“‘?;-•’—…[]!"
-phrases_to_strip = ["'s", "'re", "'ve", "'t", "[0]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]"]
+characters_to_strip = "–!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~—‘’“”•… "
+# http regex from https://www.makeuseof.com/regular-expressions-validate-url/
+http_url_regex = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+# url regex adapted from https://uibakery.io/regex-library/url-regex-python
+url_regex = "^(((http|https)://)|(www\\.))[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+# non http one from https://uibakery.io/regex-library/url-regex-python is less restrictive and matches on file names and such too
+stop_words = [sw.translate(str.maketrans('', '', string.punctuation)) for sw in stopwords.words('english')]
 
 
-def strip_characters(word):
-    word_length = len(word)
-    word = word.replace("’", "'")
-    while True:
-        for phrase in phrases_to_strip:
-            if word.endswith(phrase):
-                word = word[:len(phrase)]
-        word = word.strip(characters_to_strip).rstrip(".")
-        if len(word) == word_length:
-            return word
-        else:
-            word_length = len(word)
+def strip_characters(in_word):
+    in_word = in_word.strip(characters_to_strip)
+    http_url_pattern = re.compile(http_url_regex)
+    url_pattern = re.compile(url_regex)
+    if http_url_pattern.match(in_word):
+        return in_word
+    elif url_pattern.match(in_word):
+        return in_word
+    wnl = WordNetLemmatizer()
+    in_word = in_word.translate(str.maketrans('', '', characters_to_strip + '0123456789'))
+    return wnl.lemmatize(in_word)
 
 
 # Check that word should be added to index
-def word_is_eligible(word):
+def word_is_eligible(in_word):
     # Length check
-    if len(word) < 3:
+    if 3 > len(in_word):
         return False
     # Starts with number
-    if word[0].isdigit():
+    if in_word.isdigit():
         return False
     # Not common english word
     if common_word_mode:
-        if word.lower() in common_words or word.lower() + "s" in common_words:
+        if in_word.lower() in common_words or in_word.lower() + "s" in common_words:
+            return False
+        if in_word.lower() in stop_words or in_word.lower() + "s" in stop_words:
             return False
     else:
-        if word.lower() in stop_words or word.lower() + "s" in stop_words:
+        if in_word.lower() in stop_words or in_word.lower() + "s" in stop_words:
             return False
     # Not SANS url
-    if word.startswith("http://") or word.startswith("https://"):
+    if in_word.startswith("http://") or in_word.startswith("https://"):
         return False
     return True
 
@@ -90,6 +101,10 @@ with open(options.input_file, "r") as f:
 # dict with word as key, set as int page nums
 word_page_num_dict = {}
 for page_idx, page in enumerate(pages):
+    if page_idx == 0:
+        continue
+    if page_idx >= len(pages)-2:
+        continue
     # Recursively replace whitespace with one singular space
     page = page.replace("\n", " ").replace("\t", " ")
     page_len = len(page)
@@ -103,6 +118,7 @@ for page_idx, page in enumerate(pages):
     page = page.strip()
     # Get words
     words = page.split(" ")
+    word_tokenized = word_tokenize(page)
     for word in words:
         # Strip some punctuation
         word = strip_characters(word).lower()
@@ -112,8 +128,10 @@ for page_idx, page in enumerate(pages):
             word_page_num_dict[word] = sorted(temp_page_set)
 
 # -- sorting logic, can alter depending on preference
-sorted_word_page_num_dict = dict(sorted(word_page_num_dict.items(), key=lambda item: (item[1], item[0])))
+#sorted_word_page_num_dict = dict(sorted(word_page_num_dict.items(), key=lambda item: (item[1], item[0])))
+sorted_word_page_num_dict = dict(sorted(word_page_num_dict.items(), key=lambda item: (item[0], item[1])))
 
+print()
 # Write output to file
 with open(options.output_file, "w") as f:
     for sorted_word_page_num_key, sorted_word_page_num_value in sorted_word_page_num_dict.items():
